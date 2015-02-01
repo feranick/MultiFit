256a80e961c8222086dc820816d9860d377a1e83
Nicola Ferralis <feranick@hotmail.com>
2015-02-01 17:32:54 -0500

    Restructured code by introduction of class Peak.

--- src/multifit.py
+++ src/multifit.py
@@ -1,4 +1,4 @@
-# Multifit v. 20150201a
+# Multifit v. 20150201b
 # Nicola Ferralis <feranick@hotmail.com>
 # The entire code is covered by GNU Public License (GPL) v.3
 
@@ -10,22 +10,10 @@ import os.path
 import getopt
 
 
-def main():
+def calculate(file, type):
     
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
-    except getopt.GetoptError:
-        # print help information and exit:
-        usage()
-        sys.exit(2)
-
-    #file = 'PPRG-498_DG_fit_5.txt'
-    file = str(sys.argv[1])
-    outfile = 'fit_' + file
-    summary = 'summary.txt'
-
-    type = int(sys.argv[2])
-
+    p = Peak(type)
+    
     fpeak1 = 1
     fpeak2 = 1
     fpeak3 = 1
@@ -33,41 +21,7 @@ def main():
     fpeak5 = 1
     fpeak6 = 0
 
-
-    if type==0:
-        typec = "PVoigt"
-        peak0 = PseudoVoigtModel(prefix="p0_")
-        peak1 = PseudoVoigtModel(prefix="p1_")
-        peak2 = PseudoVoigtModel(prefix="p2_")
-        peak3 = PseudoVoigtModel(prefix="p3_")
-        peak4 = PseudoVoigtModel(prefix="p4_")
-        peak5 = PseudoVoigtModel(prefix="p5_")
-        if fpeak6!=0:
-            peak6 = PseudoVoigtModel(prefix="p6_")
-    else:
-        if type == 1:
-            typec = "Lor"
-            peak0 = LorentzianModel(prefix="p0_")
-            peak1 = LorentzianModel(prefix="p1_")
-            peak2 = LorentzianModel(prefix="p2_")
-            peak3 = LorentzianModel(prefix="p3_")
-            peak4 = LorentzianModel(prefix="p4_")
-            peak5 = LorentzianModel(prefix="p5_")
-            if fpeak6!=0:
-                peak6 = LorentzianModel(prefix="p6_")
-        else:
-            typec = "Gauss"
-            peak0 = GaussianModel(prefix="p0_")
-            peak1 = GaussianModel(prefix="p1_")
-            peak2 = GaussianModel(prefix="p2_")
-            peak3 = GaussianModel(prefix="p3_")
-            peak4 = GaussianModel(prefix="p4_")
-            peak5 = GaussianModel(prefix="p5_")
-            if fpeak6!=0:
-                peak6 = GaussianModel(prefix="p6_")
-
-
-    pars = peak0.make_params()
+    pars = p.peak0.make_params()
     pars['p0_center'].set(1100)
     pars['p0_sigma'].set(45, min=40)
     pars['p0_amplitude'].set(500, min=0)
@@ -75,7 +29,7 @@ def main():
         pars['p0_fraction'].set(0.5, min = 0, max = 1)
 
     if fpeak1!=0:
-        pars.update(peak1.make_params())
+        pars.update(p.peak1.make_params())
         pars['p1_center'].set(1250)
         pars['p1_sigma'].set(45, min=40)
         pars['p1_amplitude'].set(1000, min=0)
@@ -83,7 +37,7 @@ def main():
             pars['p1_fraction'].set(0.5, min = 0, max = 1)
 
     if fpeak2!=0:
-        pars.update(peak2.make_params())
+        pars.update(p.peak2.make_params())
         pars['p2_center'].set(1330)
         pars['p2_sigma'].set(80, min=40)
         pars['p2_amplitude'].set(5000, min=0)
@@ -91,7 +45,7 @@ def main():
             pars['p2_fraction'].set(0.5, min = 0, max = 1)
 
     if fpeak3!=0:
-        pars.update(peak3.make_params())
+        pars.update(p.peak3.make_params())
         pars['p3_center'].set(1420)
         pars['p3_sigma'].set(40, min=20)
         pars['p3_amplitude'].set(300, min=0)
@@ -99,7 +53,7 @@ def main():
             pars['p3_fraction'].set(0.5, min = 0, max = 1)
 
     if fpeak4!=0:
-        pars.update(peak4.make_params())
+        pars.update(p.peak4.make_params())
         pars['p4_center'].set(1500)
         pars['p4_sigma'].set(40, min=20)
         pars['p4_amplitude'].set(300, min=0)
@@ -107,7 +61,7 @@ def main():
             pars['p4_fraction'].set(0.5, min = 0, max = 1)
 
     if fpeak5!=0:
-        pars.update(peak5.make_params())
+        pars.update(p.peak5.make_params())
         pars['p5_center'].set(1590)
         pars['p5_sigma'].set(40, min=20)
         pars['p5_amplitude'].set(2000, min=0)
@@ -115,26 +69,26 @@ def main():
             pars['p5_fraction'].set(0.5, min = 0, max = 1)
 
     if fpeak6!=0:
-        pars.update(peak6.make_params())
+        pars.update(p.peak6.make_params())
         pars['p6_center'].set(1680)
         pars['p6_sigma'].set(40, min=30)
         pars['p6_amplitude'].set(1000, min=0)
         if type ==0:
             pars['p6_fraction'].set(0.5, min = 0, max = 1)
 
-    mod = peak0
+    mod = p.peak0
     if fpeak1!=0:
-        mod = mod + peak1
+        mod = mod + p.peak1
     if fpeak2!=0:
-        mod = mod + peak2
+        mod = mod + p.peak2
     if fpeak3!=0:
-        mod = mod + peak3
+        mod = mod + p.peak3
     if fpeak4!=0:
-        mod = mod + peak4
+        mod = mod + p.peak4
     if fpeak5!=0:
-        mod = mod + peak5
+        mod = mod + p.peak5
     if fpeak6!=0:
-        mod = mod + peak6
+        mod = mod + p.peak6
 
 
     ########################################
@@ -154,18 +108,21 @@ def main():
     #    ex_str = '{:} = {:f}'.format(key, out.best_values[key])
     #    print(ex_str)
 
+    outfile = 'fit_' + file
+    summary = 'summary.txt'
+
     if (fpeak1 ==1 & fpeak2 ==1 & fpeak5 ==1):
         print('\nD5/G = {:f}'.format(out.best_values['p1_amplitude']/out.best_values['p5_amplitude']))
         print('(D4+D5)/G = {:f}'.format((out.best_values['p0_amplitude']+out.best_values['p1_amplitude'])/out.best_values['p5_amplitude']))
         print('D1/G = {:f}'.format(out.best_values['p2_amplitude']/out.best_values['p5_amplitude']))
-        print('Fit type: {:}\n'.format(typec))
+        print('Fit type: {:}\n'.format(p.typec))
       
     if (fpeak1 ==1 & fpeak2 ==1 & fpeak5 ==1):
         with open(outfile, "a") as text_file:
             text_file.write('\nD5/G = {:f}'.format(out.best_values['p1_amplitude']/out.best_values['p5_amplitude']))
             text_file.write('\n(D4+D5)/G = {:f}'.format((out.best_values['p0_amplitude']+out.best_values['p1_amplitude'])/out.best_values['p5_amplitude']))
             text_file.write('\nD1/G = {:f}'.format(out.best_values['p2_amplitude']/out.best_values['p5_amplitude']))
-            text_file.write('Fit type: {:}\n'.format(typec))
+            text_file.write('Fit type: {:}\n'.format(self.typec))
 
 
     if os.path.isfile(summary) == False:
@@ -186,31 +143,31 @@ def main():
             sum_file.write('{:f}\t'.format(out.best_values['p1_amplitude']/out.best_values['p5_amplitude']))
             sum_file.write('{:f}\t'.format((out.best_values['p0_amplitude']+out.best_values['p1_amplitude'])/out.best_values['p5_amplitude']))
             sum_file.write('{:f}\t'.format(out.best_values['p2_amplitude']/out.best_values['p5_amplitude']))
-            sum_file.write('{:}\n'.format(typec))
+            sum_file.write('{:}\n'.format(p.typec))
 
     #plt.plot(x, out.init_fit, 'k--')
     plot(x, out.best_fit, 'r-', label='fit')
 
-    y0 = peak0.eval(x = x, **out.best_values)
+    y0 = p.peak0.eval(x = x, **out.best_values)
     plt.plot(x,y0,'g')
 
     if (fpeak1 ==1):
-        y1 = peak1.eval(x = x, **out.best_values)
+        y1 = p.peak1.eval(x = x, **out.best_values)
         plt.plot(x,y1,'g',linewidth=2.0)
     if (fpeak2 ==1):
-        y2 = peak2.eval(x = x, **out.best_values)
+        y2 = p.peak2.eval(x = x, **out.best_values)
         plt.plot(x,y2,'g')
     if (fpeak3 ==1):
-        y3 = peak3.eval(x = x, **out.best_values)
+        y3 = p.peak3.eval(x = x, **out.best_values)
         plt.plot(x,y3,'g')
     if (fpeak4 ==1):
-        y4 = peak4.eval(x = x, **out.best_values)
+        y4 = p.peak4.eval(x = x, **out.best_values)
         plt.plot(x,y4,'g')
     if (fpeak5 ==1):
-        y5 = peak5.eval(x = x, **out.best_values)
+        y5 = p.peak5.eval(x = x, **out.best_values)
         plt.plot(x,y5,'g',linewidth=2.0)
     if (fpeak6 ==1):
-        y6 = peak6.eval(x = x, **out.best_values)
+        y6 = p.peak6.eval(x = x, **out.best_values)
         plt.plot(x,y6,'g')
 
 
@@ -222,6 +179,54 @@ def main():
     grid(True)
     #savefig("test.png")
     show()
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+    except getopt.GetoptError:
+        # print help information and exit:
+        usage()
+        sys.exit(2)
+    
+    #file = 'PPRG-498_DG_fit_5.txt'
+    file = str(sys.argv[1])
+    type = int(sys.argv[2])
+
+    calculate(file, type)
+
+class Peak:
+
+    def __init__(self, type):
+        if type==0:
+            self.peak0 = PseudoVoigtModel(prefix="p0_")
+            self.peak1 = PseudoVoigtModel(prefix="p1_")
+            self.peak2 = PseudoVoigtModel(prefix="p2_")
+            self.peak3 = PseudoVoigtModel(prefix="p3_")
+            self.peak4 = PseudoVoigtModel(prefix="p4_")
+            self.peak5 = PseudoVoigtModel(prefix="p5_")
+            self.peak6 = PseudoVoigtModel(prefix="p6_")
+            self.typec = "PVoigt"
+        else:
+            if type == 1:
+                self.peak0 = LorentzianModel(prefix="p0_")
+                self.peak1 = LorentzianModel(prefix="p1_")
+                self.peak2 = LorentzianModel(prefix="p2_")
+                self.peak3 = LorentzianModel(prefix="p3_")
+                self.peak4 = LorentzianModel(prefix="p4_")
+                self.peak5 = LorentzianModel(prefix="p5_")
+                self.peak6 = LorentzianModel(prefix="p6_")
+                self.typec = "Lorentz"
+            else:
+                self.peak0 = GaussianModel(prefix="p0_")
+                self.peak1 = GaussianModel(prefix="p1_")
+                self.peak2 = GaussianModel(prefix="p2_")
+                self.peak3 = GaussianModel(prefix="p3_")
+                self.peak4 = GaussianModel(prefix="p4_")
+                self.peak5 = GaussianModel(prefix="p5_")
+                self.peak6 = GaussianModel(prefix="p6_")
+                self.typec = "Gauss"
+
+
 
 if __name__ == "__main__":
     sys.exit(main())
