686783cb19a28933f7d124c77bcf8af12108e397
Nicola Ferralis <feranick@hotmail.com>
2015-11-03 17:58:08 -0500

    Renamed NumPeaks to numPeaks for consistency with other variables.

--- multifit_3/multifit.py
+++ multifit_3/multifit.py
@@ -22,9 +22,9 @@ import multiprocessing as mp
 ''' Program definitions and configuration variables '''
 ####################################################################
 class defPar:
-    version = '3-20151103a'
+    version = '3-20151103b'
     ### Define number of total peaks (do not change: this is read from file)
-    NumPeaks = 0
+    numPeaks = 0
     ### Name input paramter file
     inputParFile = 'input_parameters.csv'
     # Save summary fitting results
@@ -68,20 +68,20 @@ def calculate(x, y, x1, y1, file, type, processMap, showPlot, lab):
         numRows = 0
         inval=[]
         for row in input:
-            defPar.NumPeaks = len(row)-1
+            defPar.numPeaks = len(row)-1
             row = [nulStrConvDigit(entry) for entry in row]
             inval.append(row)
             numRows +=1
         inputFile.close()
-    inv = resize(inval, [numRows, defPar.NumPeaks+1])
+    inv = resize(inval, [numRows, defPar.numPeaks+1])
     fpeak = []
 
     # define active peaks
-    for i in range(1, defPar.NumPeaks+1):
+    for i in range(1, defPar.numPeaks+1):
         fpeak.extend([int(inv[1,i])])
 
     p = Peak(type)
-    print (' Fitting with ' + str(defPar.NumPeaks) + ' (' + p.typec + ') peaks')
+    print (' Fitting with ' + str(defPar.numPeaks) + ' (' + p.typec + ') peaks')
     if(defPar.peakPosOpt==True):
         print(' Peak Search Optimization: ON\n')
     else:
@@ -89,7 +89,7 @@ def calculate(x, y, x1, y1, file, type, processMap, showPlot, lab):
     
     ### Initialize parameters for fit.
     pars = p.peak[0].make_params()
-    for i in range (0, defPar.NumPeaks):
+    for i in range (0, defPar.numPeaks):
         if fpeak[i]!=0:
 
             fac1 = 2
@@ -116,7 +116,7 @@ def calculate(x, y, x1, y1, file, type, processMap, showPlot, lab):
 
     ### Add relevant peak to fitting procedure.
     mod = p.peak[0]
-    for i in range (1,defPar.NumPeaks):
+    for i in range (1,defPar.numPeaks):
         if fpeak[i]!=0:
             mod += p.peak[i]
 
@@ -223,8 +223,8 @@ def calculate(x, y, x1, y1, file, type, processMap, showPlot, lab):
         ax.plot(x, out.best_fit, 'r-', label='fit')
         y0 = p.peak[0].eval(x = x, **out.best_values)
         #ax.plot(x,y0,'g')
-        y = [None]*(defPar.NumPeaks + 1)
-        for i in range (0,defPar.NumPeaks):
+        y = [None]*(defPar.numPeaks + 1)
+        for i in range (0,defPar.numPeaks):
             if (fpeak[i] ==1):
                 y[i] = p.peak[i].eval(x = x, **out.best_values)
                 if (i==2 or i==6):
@@ -436,27 +436,27 @@ class Peak:
     ### Define the typology of the peak
     def __init__(self, type):
         
-        self.peak= [None]*(defPar.NumPeaks)
+        self.peak= [None]*(defPar.numPeaks)
         
         if type==0:
-            for i in range (0,defPar.NumPeaks):
+            for i in range (0,defPar.numPeaks):
                 self.peak[i] = PseudoVoigtModel(prefix="p"+ str(i) +"_")
             self.typec = "PseudoVoigt"
         elif type == 1:
-            for i in range (0,defPar.NumPeaks):
+            for i in range (0,defPar.numPeaks):
                 self.peak[i] = GaussianModel(prefix="p"+ str(i) +"_")
             self.typec = "Gauss"
         elif type == 2:
-            for i in range (0,defPar.NumPeaks):
+            for i in range (0,defPar.numPeaks):
                 self.peak[i] = LorentzianModel(prefix="p"+ str(i) +"_")
             self.typec = "Lorentz"
         elif type ==3:
-            for i in range (0,defPar.NumPeaks):
+            for i in range (0,defPar.numPeaks):
                 self.peak[i] = VoigtModel(prefix="p"+ str(i) +"_")
             self.typec = "Voigt"
         else:
             print("Warning: type undefined. Using PseudoVoigt")
-            for i in range (0,defPar.NumPeaks):
+            for i in range (0,defPar.numPeaks):
                 self.peak[i] = PseudoVoigtModel(prefix="p"+ str(i) +"_")
             self.typec = "PVoigt"
 
